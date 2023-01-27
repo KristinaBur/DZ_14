e738ead6102e866f5bfcbab15fbf8afc2221b66d
«Kristina» <kristinka007_92@mail.ru>
2023-01-27 15:59:11 +0300

    Задача 54 и 56

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
 9 5 3 2
 8 4 4 2 */
 
-Console.Write("Введите количество строк: ");
+/*Console.Write("Введите количество строк: ");
 int rows = int.Parse(Console.ReadLine()!);
 
 Console.Write("Введите количество столбцов: ");
@@ -60,3 +60,73 @@ void SortArray(int[,] array)
         }
     }
 }
+*/
+/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);          
+
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);      
+Console.WriteLine();
+int[,] array = GetArray(rows, columns, 0, 10); 
+PrintArray(array);
+Console.WriteLine(); 
+SumLine(array);
+
+
+//---------Методы----------
+int[,] GetArray(int m, int n, int minValue, int maxValue){ 
+    int[,] result = new int[m,n]; 
+    for(int i = 0; i < m; i++) 
+    { 
+    for(int j = 0; j < n; j++) 
+    { 
+    result[i,j] = new Random().Next(minValue, maxValue + 1); 
+    } 
+    } 
+    return result; 
+} 
+ 
+void PrintArray(int[,] array)
+{ 
+    for(int i = 0; i < array.GetLength(0); i++) 
+    { 
+    for(int j = 0; j < array.GetLength(1); j++) 
+    { 
+    Console.Write($"{array[i,j]} "); 
+    } 
+    Console.WriteLine(); 
+    } 
+} 
+
+void SumLine(int[,] array)
+{
+    int index = 0;
+    int minsum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i,j];
+        }
+        if (i==0)
+        {
+            minsum=sum;
+        }
+        else if (sum<minsum)
+        {
+            minsum=sum;
+            index = i;
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой элементов -> {index+1} ");
+}
+
