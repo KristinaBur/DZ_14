3b909c7a04b64954cfb2b519f85519110e3e1c2e
«Kristina» <kristinka007_92@mail.ru>
2023-01-29 14:46:54 +0300

    Задача 54, 56, 58

--- Program.cs
+++ Program.cs
@@ -69,7 +69,7 @@ void SortArray(int[,] array)
 5 2 6 7
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
 
-Console.Write("Введите количество строк: ");
+/*  Console.Write("Введите количество строк: ");
 int rows = int.Parse(Console.ReadLine()!);          
 
 Console.Write("Введите количество столбцов: ");
@@ -124,9 +124,91 @@ void SumLine(int[,] array)
         else if (sum<minsum)
         {
             minsum=sum;
-            indexLine = i;
+            indexLine = i;   // indexLine++;  - одно и тоже
         }
     }
     Console.WriteLine($"Строка с наименьшей суммой элементов -> {indexLine+1} ");
 }
+
+*/
+
+/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18  */
+
+Console.Write("Введите количество строк первой матрицы: ");
+int rows1 = int.Parse(Console.ReadLine()!);          
+
+Console.Write("Введите количество столбцов первой матрицы: ");
+int columns1 = int.Parse(Console.ReadLine()!);  
+
+Console.Write("Введите количество строк второй матрицы: ");
+int rows2 = int.Parse(Console.ReadLine()!);          
+
+Console.Write("Введите количество столбцов второй матрицы: ");
+int columns2 = int.Parse(Console.ReadLine()!); 
+
+int[,] array1 = GetArray(rows1, columns1, 0, 10); 
+Console.WriteLine();
+int[,] array2 = GetArray(rows2, columns2, 0, 10); 
+Console.WriteLine("Первая матрица:");
+PrintArray(array1);
+Console.WriteLine();
+Console.WriteLine("Вторая матрица:"); 
+PrintArray(array2);
+Console.WriteLine(); 
+
+if (array1.GetLength(0) != array2.GetLength(1))
+{
+    Console.WriteLine("Произведение двух матриц выполнить нельзя ");
+    return;
+}
+
+int[,] resultArray = new int[rows1, columns1];
+
+for (int i = 0; i < array1.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2.GetLength(1); j++)
+                {
+                    resultArray[i, j] = 0;
+                    for (int k = 0; k < array1.GetLength(1); k++)
+                    {
+                       resultArray[i, j] += array1[i, k] * array2[k, j];
+                    }
+                }
+            }
+            
+Console.WriteLine("Произведение двух матриц:");           
+PrintArray(resultArray);
+
+
+//---------Методы----------
+int[,] GetArray(int m, int n, int minValue, int maxValue){ 
+    int[,] result = new int[m,n]; 
+    for(int i = 0; i < m; i++) 
+    { 
+    for(int j = 0; j < n; j++) 
+    { 
+    result[i,j] = new Random().Next(minValue, maxValue + 1); 
+    } 
+    } 
+    return result; 
+} 
+ 
+void PrintArray(int[,] array)
+{ 
+    for(int i = 0; i < array.GetLength(0); i++) 
+    { 
+    for(int j = 0; j < array.GetLength(1); j++) 
+    {   
+    Console.Write($"{array[i,j]} "); 
+    } 
+    Console.WriteLine(); 
+    } 
+} 
+
 
